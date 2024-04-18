73caea9bf315bd5c7ad5255f98d75dee9f09fa06
Ivan Fuentes <ivangabrielf971@gmail.com>
2024-04-18 14:59:41 -0300

    Add main page

--- src/app/climafoto/page.tsx
+++ src/app/climafoto/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const WeatherForecast = () => {
     const [location, setLocation] = useState('');
     const [forecast, setForecast] = useState([]);
     const [images, setImages] = useState([]);
     const [error, setError] = useState('');
+
+    const isMobile = useMediaQuery({ maxWidth: 768 }); // Define como móvil si el ancho de la pantalla es menor o igual a 768px
 
     const handleLocationChange = (event) => {
         setLocation(event.target.value);
@@ -71,21 +74,27 @@ const WeatherForecast = () => {
         }
     };
 
+    const getColumnCount = () => {
+        return isMobile ? 2 : 4;
+    };
+
     return (
-        <div>
-            <h1>Weather Forecast</h1>
-            <input
-                type="text"
-                className='text-black'
-                placeholder="Enter location (e.g., Diamante)"
-                value={location}
-                onChange={handleLocationChange}
-            />
-            <button onClick={search}>Buscar</button>
+        <div className="h-full w-full mx-auto bg-gray-100 text-black text-center">
+            <h1 className="text-2xl font-bold mb-4">Api Clima con fotos</h1>
+            <div className="mb-4 flex flex-col items-center">
+                <input
+                    type="text"
+                    className='text-black border border-gray-300 px-4 py-2 rounded-lg w-64 focus:outline-none focus:border-blue-500 mb-2'
+                    placeholder="Enter location (e.g., Diamante)"
+                    value={location}
+                    onChange={handleLocationChange}
+                />
+                <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600" onClick={search}>Buscar</button>
+            </div>
 
-            {error && <p>{error}</p>}
+            {error && <p className="text-red-500 mb-4">{error}</p>}
 
-            <ul>
+            <ul className="text-left">
                 {forecast.map((day) => (
                     <li key={day.date}>
                         <p>Fecha: {day.date}</p>
@@ -95,11 +104,11 @@ const WeatherForecast = () => {
                 ))}
             </ul>
 
-            <div>
+            <div className={`grid grid-cols-${getColumnCount()} gap-4 mt-4 px-2`}>
                 {images.map((image) => (
                     // Aquí se utiliza la función getWeatherImageType para determinar el tipo de imagen
                     // Cambia la lógica aquí según tus necesidades para mostrar diferentes tipos de imágenes
-                    <img key={image.id} src={getWeatherImageType(forecast[0]?.day.maxtemp_c) === "soleado" ? image.urls.sunny : image.urls.default} alt="" />
+                    <img key={image.id} src={getWeatherImageType(forecast[0]?.day.maxtemp_c) === "soleado" ? image.urls.thumb : image.urls.default} alt="" className="w-full" />
                 ))}
             </div>
         </div>
